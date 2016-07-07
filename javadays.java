670b68b759e399e70db654da1482ccc2f44011ed
betping <betping1993@gmail.com>
2016-07-07 19:13:20 +0530

    Add files via upload

---
+++ javadays.java
@@ -0,0 +1,177 @@
+import java.util.Scanner;
+
+public class javadays{
+     
+    public static void main(String[] args)
+
+    {
+        String Month;
+
+        Scanner month = new Scanner(System.in);
+
+        System.out.println("java Days");
+
+        System.out.print("Enter the Month: ");
+
+        
+        Month = month.next();
+
+     
+        if ( "January" != null);
+      {
+
+                 
+
+            System.out.printf("The month you chose is %s", Month);
+
+            System.out.print("It has 31 day");
+        }
+     
+
+         if ( "February" != null);
+
+        {
+
+                 
+            System.out.printf("The month you chose is %s", Month);
+
+            System.out.print("It has 27 days");
+
+        }
+
+         
+
+         if ( "March" != null)
+        {
+
+                 
+
+            System.out.printf("The month you chose is %s", Month);
+            System.out.print("It has 31 days");
+
+        }
+        
+        if ("April" != null)
+
+        {
+
+                 
+
+            System.out.printf("The month you chose is %s", Month);
+
+            System.out.print("It has 31 days");
+
+        }
+
+         
+
+        else if ( "May" != null)
+
+        {
+
+                 
+
+            System.out.printf("The month you chose is %s", Month);
+            System.out.print("It has 31 days");
+
+        }
+
+         
+
+        else if ("June" != null)
+
+        {
+                 
+
+            System.out.printf("The month you chose is %s", Month);
+
+            System.out.print("It has 31 days");
+
+        }
+
+         
+         if ( "July" != null)
+
+        {
+                 
+
+            System.out.printf("The month you chose is %s", Month);
+
+            System.out.print("It has 31 days");
+
+        }
+
+         
+
+        else if ( "August" != null)
+
+        {
+
+                 
+
+            System.out.printf("The month you chose is %s", Month);
+
+            System.out.print("It has 31 days");
+
+        }
+
+         
+
+        else if ( "September" != null)
+
+        {
+
+                 
+            System.out.printf("The month you chose is %s", Month);
+
+            System.out.print("It has 30 days");
+
+        }
+
+         
+
+        else if ( "October" != null)
+
+        {
+
+                 
+
+            System.out.printf("The month you chose is %s", Month);
+
+            System.out.print("It has 31 days");
+
+        }
+
+         
+
+        else if ("November" != null)
+
+        {
+
+                 
+
+            System.out.printf("The month you chose is %s", Month);
+
+            System.out.print("It has 30 days");
+
+        }
+
+         
+
+        else if ("December" != null );
+
+        {
+
+                 
+
+            System.out.printf("The month you chose is %s", Month);
+
+            System.out.print("It has 31 days");
+
+        }
+
+     
+
+    }
+ 
+    }
